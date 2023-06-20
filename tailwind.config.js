4e5dc4afed035943d3addf9c7cd89aa7750ec329
Ennygenius <inuenike@gmail.com>
2023-06-20 22:52:54 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	content: ['./src/**/*.{html,js}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				poppins: ['Poppins'],
+				cursive: [ 'Pacifico,cursive'],
+			},
+		},
+	},
+	plugins: [],
+};
